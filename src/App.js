0e9c8bf657960f4218caa59c054e8a23efe30750
Gabriel Miroiu <miroiugabriel1231@gmail.com>
2021-01-27 20:40:49 +0200

    Initial

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './components/ColorGame.js';
-import './App.css';
-import ColorGame from './components/ColorGame.js';
-
-const App = () => {
-	return <ColorGame />;
-};
-
-export default App;
